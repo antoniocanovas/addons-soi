67505dcaa64c5407b93273cd710b9cd38b36a511
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-04-21 18:13:44 +0200

    report v0

---
+++ reports_soillums/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 IC - Pedro Guirao
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": " Factura soillums",
+    "summary": "Reports Soillums",
+    "version": "1.0.0.0.0",
+    "category": "Reports",
+    "author": "SerinCloud, ",
+    "website": "https://ingeniriacloud.com",
+    "license": "AGPL-3",
+    "depends": ['account'],
+    "data": [
+        "views/report_account_move_changes.xml",
+    ],
+    "installable": True,
+}
